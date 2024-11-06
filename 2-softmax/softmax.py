ad907eb0de4834536fd015c7a7d3b7b633535cd2
smile2game <2426827419@qq.com>
2024-11-06 15:12:40 +0800

    result

--- 2-softmax/softmax.py
+++ 2-softmax/softmax.py
@@ -77,3 +77,38 @@ x = torch.randn(1823, 781, device='cuda')
 y_triton = softmax(x)
 y_torch = torch.softmax(x, axis=1)
 assert torch.allclose(y_triton, y_torch), (y_triton, y_torch)
+
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['N'],  # 用作图表x轴的参数名
+        x_vals=[128 * i for i in range(2, 100)],  # `x_name`的不同可能值
+        line_arg='provider',  # 其值对应图表中不同线条的参数名
+        line_vals=[
+            'triton',
+            'torch-native',
+            'torch-jit',
+        ],  # `line_arg`的可能值
+        line_names=[
+            "Triton",
+            "Torch (native)",
+            "Torch (jit)",
+        ],  # 线条的标签名
+        styles=[('blue', '-'), ('green', '-'), ('green', '--')],  # 线条样式
+        ylabel="GB/s",  # y轴的标签名
+        plot_name="softmax-performance",  # 图表的名称。也用作保存图表的文件名。
+        args={'M': 4096},  # 不在`x_names`和`y_name`中的函数参数值
+    ))
+def benchmark(M, N, provider):
+    x = torch.randn(M, N, device='cuda', dtype=torch.float32)
+    quantiles = [0.5, 0.2, 0.8]
+    if provider == 'torch-native':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch.softmax(x, axis=-1), quantiles=quantiles)
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: softmax(x), quantiles=quantiles)
+    if provider == 'torch-jit':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: naive_softmax(x), quantiles=quantiles)
+    gbps = lambda ms: 2 * x.nelement() * x.element_size() * 1e-9 / (ms * 1e-3)
+    return gbps(ms), gbps(max_ms), gbps(min_ms)
+
+
+benchmark.run(print_data=True, show_plots=True, save_path='./output')
